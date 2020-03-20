541652f2a6e29a325f3e92fcd707261c90593507
tahirbhalli <tahirbhalli16@gmail.com>
2020-03-20 21:51:45 +0500

    set exemples

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -3,11 +3,21 @@ describe 'Enumerable' do
   it 'enum testing' do
     arr = [1, 2, 3, 4, 5]
     arr.my_any?.equal? arr.any?
+  end
+  it 'enum testing' do
     [1, 2, 3, 4, 5].my_select.class.equal? [1, 2, 3, 4, 5].select.class
-    true_array = [nil, false, true, []]
-    true_array.my_any? .equal? true_array.any?
-    [].all?.equal? [].my_all?
-    [].none?.equal? [].my_none?
+  end
+  it 'enum testing' do
     [nil, false, nil, false].my_any?.equal? [nil, false, nil, false].any?
+  end
+  it 'enum testing' do
+    [].none?.equal? [].my_none?
+  end
+  it 'enum testing' do
+    [].all?.equal? [].my_all?
+  end
+  it 'enum testing' do
+    true_array = [nil, false, true, []]
+    true_array.my_any?.equal? true_array.any?
   end
 end
